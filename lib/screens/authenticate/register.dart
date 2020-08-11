6c060ec7245022b003b2ae747dbc067468215f3d
Harun Shaban <42933471+harunshaban@users.noreply.github.com>
2020-08-11 15:54:24 +0200

    created validation in register view

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -13,6 +13,7 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
 
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   //text field state
   String email = '';
@@ -39,10 +40,12 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0,),
               TextFormField(
+                validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 onChanged: (val){
                   setState(() => email = val);
                 },
@@ -50,6 +53,7 @@ class _RegisterState extends State<Register> {
               SizedBox(height: 20.0,),
               TextFormField(
                 obscureText: true,
+                validator: (val) => val.length < 6 ? 'Enter a password 6+ char long' : null,
                 onChanged: (val){
                   setState(() => password  = val);
                 },
@@ -62,8 +66,10 @@ class _RegisterState extends State<Register> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if(_formKey.currentState.validate()){
+                    print(email);
+                    print(password);
+                  }
                 },
               ),
             ],
